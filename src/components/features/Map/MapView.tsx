4362fbc7545c087ac97ed650148035821f5ab701
farhanibnmustafa <farhanibnmustafa@gmail.com>
2026-04-05 02:50:47 +0600

    fix: remove retina parameter from map tile layer URL

--- src/components/features/Map/MapView.tsx
+++ src/components/features/Map/MapView.tsx
@@ -127,7 +127,7 @@ export default function MapView({ mode, onLocationSelect, userLocation }: MapVie
                 className={styles.map}
             >
                 <TileLayer
-                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 />
                 
